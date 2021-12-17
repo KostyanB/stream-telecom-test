1ef37937ae22cbf93a69ca1b97de9e7dbaba7564
Konstantin Berezko <berezkok72@gmail.com>
2021-12-17 13:45:10 +0500

    fix(streamTel):refactore code

--- src/modules/globalSrore.js
+++
@@ -1,4 +0,0 @@
-import Store from './useStore';
-
-const globalStore = new Store();
-export default globalStore;
--- src/modules/useStore.js
+++ src/modules/useStore.js
@@ -3,22 +3,19 @@ class Store {
     this.store = store
   }
 
+  clearStore() {
+    this.store.length = 0;
+  }
+
   getStore() {
     return this.store;
   }
 
   setStore(data) {
-    this.store.length = 0;
+    this.clearStore();
     this.store.push(...data);
   }
 }
 
 export const globalStore = new Store();
-
-class Local extends Store {
-  clearStore() {
-    this.store.length = 0;
-  }
-}
-
-export const localStore = new Local();
+export const localStore = new Store();
